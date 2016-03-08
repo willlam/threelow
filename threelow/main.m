7e89723ec16bf441bf750a347b6e50371678f875
will <will@willlam.net>
2016-03-08 18:41:48 -0500

    Initial Commit

---
+++ threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  threelow
+//
+//  Created by William Lam on 2016-03-08.
+//  Copyright © 2016 William Lam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
